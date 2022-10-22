a0b07af4c597bf33e00e5c5e74fa73099bd62cdf
Anna <ananyaa.koundal2003@gmail.com>
2022-10-23 01:34:03 +0530

    Update prac9.c

--- prac9.c
+++ prac9.c
@@ -1,59 +1,42 @@
-#include<iostream>
-#include<stdio.h>
-using namespace std;
-int main()
-{
-int bt[20],p[20],wt[20],tat[20],pr[20],i,j,n,total=0,pos,temp;
-float avg_wt,avg_tat;
-cout<<"\nEnter Total Number of Process:";
-cin>>n;
-cout<<"\nEnter Burst Time and Priority:\n";
-for(i=0;i<n;i++)
-{
-cout<<"\nProcess "<<i+1<<"-> ";
-cout<<"Burst Time: ";
-cin>>bt[i];
-cout<<"Priority: ";
-cin>>pr[i];
-p[i]=i+1; 
-}
-for(i=0;i<n;i++)
-{
-pos=i;
-for(j=i+1;j<n;j++)
-{
-if(pr[j]<pr[pos])
-pos=j;
-}
-temp=pr[i];
-pr[i]=pr[pos];
-pr[pos]=temp;
-temp=bt[i];
-bt[i]=bt[pos];
-bt[pos]=temp;
-temp=p[i];
-p[i]=p[pos];
-p[pos]=temp;
-}
-wt[0]=0; 
-for(i=1;i<n;i++)
-{
-wt[i]=0;
-for(j=0;j<i;j++)
-wt[i]+=bt[j];
-total+=wt[i];
-}
-avg_wt=(float)total/n; 
-total=0;
-cout<<"\nProcess\t Burst Time \tWaiting Time\tTurn Around Time";
-for(i=0;i<n;i++)
-{
-tat[i]=bt[i]+wt[i];
-total+=tat[i];
-cout<<"\n  P:"<<p[i]<<"\t\t  "<<bt[i]<<"\t\t\t\t"<<wt[i]<<"\t\t\t\t"<<tat[i];
-}
-avg_tat=(float)total/n; 
-cout<<"\n\nAverage Waiting Time: "<<avg_wt;
-cout<<"\nAverage Turnaround Time: "<<avg_tat<<endl;
-return 0;
+#include <stdlib.h>
+#include <stdio.h>
+int main(){
+	int arrive[10], burst[20],rt[20],wait[20],turn[20], pr[20],i,j,n;
+	int time, sum_wait=0, sum_turn=0, smallest, remain;
+	printf("\nEnter the number of processes: ");
+	scanf("%d", &n);
+	remain=n;
+	for(i=0; i<n; i++){
+		printf("\tEProcess %d : \n", i+1);
+		printf("\tEnter the arrive time : ");
+		scanf("%d", &arrive[i]);
+		printf("\tEnter the burst time : ");
+		scanf("%d", &burst[i]);
+		printf("\tEnter the priority : ");
+		scanf("%d", &pr[i]);
+		rt[i]=burst[i];
+	}
+	pr[9]=11;
+	printf("\n\nProcess \t|Waiting Time \t|Turn Around Time\n");
+	for(time=0;remain!=0;time++)
+	{
+		smallest=9;
+		for(i=0;i<n;i++){
+			if(arrive[i]<=time && pr[i]<pr[smallest] && rt[i]>0){
+				smallest=i;
+			}
+		}
+		rt[smallest]--;
+		if(rt[smallest]==0)
+		{
+			remain--;
+			printf("%d \t\t   %d  \t\t   %d\n", smallest+1,time+1-arrive[smallest]-burst[smallest], time+1-arrive[smallest]);
+			sum_turn+=time+1-arrive[smallest];
+			sum_wait+=time+1-arrive[smallest]-burst[smallest];
+		}
+	}
+	printf("\nAverage Waiting Time: %f",  sum_wait*1.0/n);
+	printf("\nAverage Turn Around Time: %f\n",sum_turn*1.0/n );
+	
+	return 0;
 }
