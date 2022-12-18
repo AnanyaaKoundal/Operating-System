47d93e08a0f7f923fbe27e318828c92a36e06ea9
Anna <ananyaa.koundal2003@gmail.com>
2022-12-18 22:43:16 +0530

    Update prac9.c

--- prac9.c
+++ prac9.c
@@ -1,42 +1,52 @@
+//Non pre emptive priority based scheduling
 #include <stdlib.h>
 #include <stdio.h>
 int main(){
-	int arrive[10], burst[20],rt[20],wait[20],turn[20], pr[20],i,j,n;
-	int time, sum_wait=0, sum_turn=0, smallest, remain;
+	int burst[20],p[20],wait[20],turn[20],pr[20],i,j,n, total=0,pos, temp;
+	float avg_wt,avg_tat;
 	printf("\nEnter the number of processes: ");
 	scanf("%d", &n);
-	remain=n;
 	for(i=0; i<n; i++){
-		printf("\tEProcess %d : \n", i+1);
-		printf("\tEnter the arrive time : ");
-		scanf("%d", &arrive[i]);
-		printf("\tEnter the burst time : ");
+		printf("\tEnter the burst time of process %d : ", i+1);
 		scanf("%d", &burst[i]);
-		printf("\tEnter the priority : ");
+		printf("\tEnter the priority of process %d : ", i+1);
 		scanf("%d", &pr[i]);
-		rt[i]=burst[i];
+		p[i]=i+1;
 	}
-	pr[9]=11;
-	printf("\n\nProcess \t|Waiting Time \t|Turn Around Time\n");
-	for(time=0;remain!=0;time++)
-	{
-		smallest=9;
-		for(i=0;i<n;i++){
-			if(arrive[i]<=time && pr[i]<pr[smallest] && rt[i]>0){
-				smallest=i;
-			}
+	for(i=0; i<n; i++){
+		pos=i;
+		for(j=i+1; j<n; j++){
+			if(pr[j]<pr[pos])
+				pos=j;
 		}
-		rt[smallest]--;
-		if(rt[smallest]==0)
-		{
-			remain--;
-			printf("%d \t\t   %d  \t\t   %d\n", smallest+1,time+1-arrive[smallest]-burst[smallest], time+1-arrive[smallest]);
-			sum_turn+=time+1-arrive[smallest];
-			sum_wait+=time+1-arrive[smallest]-burst[smallest];
-		}
+		temp=pr[i];
+		pr[i]=pr[pos];
+		pr[pos]=temp;
+		temp=burst[i];
+		burst[i]=burst[pos];
+		burst[pos]=temp;
+		temp=p[i];
+		p[i]=p[pos];
+		p[pos]=temp;
 	}
-	printf("\nAverage Waiting Time: %f",  sum_wait*1.0/n);
-	printf("\nAverage Turn Around Time: %f\n",sum_turn*1.0/n );
+	wait[0]=0;
+	for(i=1; i<n; i++){
+		wait[i]=0;
+		for(j=0; j<i; j++)
+			wait[i]+=burst[j];
+		total+=wait[i];
+	}
+	float avw=(float)total/n;
+	total=0;
+	printf("\n\nProcess \t|Burst Time \t|Waiting Time \t|Turn Around Time\n");
+	for(i=0; i<n; i++){
+		turn[i]=burst[i]+wait[i];
+		total+=turn[i];
+		printf("%d \t\t   %d  \t\t   %d  \t\t   %d\n", p[i],burst[i],wait[i], turn[i]);
+	}
+	float avt=(float)total/n;
+	printf("\nAverage Waiting Time: %f", avw );
+	printf("\nAverage Turn Around Time: %f\n", avt);
 	
 	return 0;
 }
